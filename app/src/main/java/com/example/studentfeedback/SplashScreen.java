2fb458d336f77f86ef89ca22ff1bbe15dbed535d
jaymadhu <jaymadhu@iu.edu>
2020-11-11 09:54:47 -0500

    splash screen

---
+++ app/src/main/java/com/example/studentfeedback/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.studentfeedback;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, SelectUniversity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
+
+    }
+
+}
